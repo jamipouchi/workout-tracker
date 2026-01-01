d5a7984e24367cc8f5ed7b65edaceae61dfe2b02
Miquel Puig Turon <miquel.puig@genesy.ai>
2026-01-01 16:49:28 +0100

    try cloudflare preset

--- fe/app.config.ts
+++ fe/app.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from '@solidjs/start/config'
+import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-    server: {
-        preset: 'static',
-        prerender: {
-            crawlLinks: true,
-            routes: ['/', '/404']
-        },
+  server: {
+    compatibilityDate: "2024-09-19",
+    preset: "cloudflare-module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
     },
-})
+    prerender: {
+      crawlLinks: true,
+      routes: ["/", "/log", "/view", "/404"],
+    },
+  },
+});
